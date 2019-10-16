2b88744f5d4ff6609ef0ed0a1a090e6fa8f17e54
Martijn Keesmaat <martijn@Martijns-MacBook-Pro.local>
2019-10-16 16:01:05 +0200

    add pitch slides

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+  import Item from "./Item.svelte";
+  let name = "world";
+  let count = 0;
+
+  function handleClick() {
+    count += 1;
+  }
+
+  let dummyData = [
+    { name: "This is a component" },
+    { name: "This is a component" },
+    { name: "This is a component" }
+  ];
+</script>
+
+<style type="text/scss">
+  $color: green;
+  $black: #000;
+
+  h1 {
+    color: $color;
+  }
+
+  div {
+    background: green;
+
+    > p {
+      color: #fff;
+    }
+  }
+</style>
+
+<h1>Hello {name} {count}!</h1>
+
+<ul>
+  {#each dummyData as { id, name }, i}
+    <Item {name} />
+  {/each}
+</ul>
+
+<button on:click={handleClick}>
+  Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
