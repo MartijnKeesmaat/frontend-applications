deb6f82058a0d8d61e0fff424032241bac30a011
Martijn <mpkeesmaat@gmail.com>
2019-10-19 19:42:55 +0200

    canvas mask(spotlight)

--- src/routes/Mask.svelte
+++ src/routes/Mask.svelte
@@ -1,5 +1,81 @@
 <script>
   import { onMount } from "svelte";
+  onMount(() => {
+    canvasApp();
+  });
+
+  function canvasApp() {
+    var canvas = document.getElementById("canvasOne");
+    var ctx = canvas.getContext("2d");
+
+    var w = (canvas.width = window.innerWidth);
+    var h = (canvas.height = window.innerHeight);
+
+    function reOffset() {
+      var BB = canvas.getBoundingClientRect();
+      offsetX = BB.left;
+      offsetY = BB.top;
+    }
+
+    var offsetX, offsetY;
+    reOffset();
+
+    window.onscroll = function(e) {
+      reOffset();
+    };
+
+    window.onresize = function(e) {
+      reOffset();
+    };
+
+    canvas.addEventListener("mousemove", mouseMove, false);
+    canvas.addEventListener("touchmove", mouseMove, false);
+
+    function draw(cx, cy, radius) {
+      ctx.save();
+      ctx.clearRect(0, 0, w, h);
+
+      var radialGradient = ctx.createRadialGradient(cx, cy, 1, cx, cy, radius);
+
+      radialGradient.addColorStop(0, "rgba(0, 0, 0, 0.3)");
+      radialGradient.addColorStop(0.65, "rgba(0, 0, 0, 0.3)");
+      radialGradient.addColorStop(1, "rgba(0, 0, 0, 0)");
+
+      ctx.beginPath();
+
+      ctx.fillStyle = "rgba(0, 0, 0, 0.9)";
+      ctx.fillRect(0, 0, w, h);
+
+      ctx.globalCompositeOperation = "destination-out";
+
+      ctx.arc(cx, cy, radius, 0, Math.PI * 2, false);
+      ctx.fillStyle = radialGradient;
+      ctx.fill();
+
+      ctx.restore();
+    }
+
+    function mouseMove(e) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      const mouseX = parseInt(e.clientX - offsetX);
+      const mouseY = parseInt(e.clientY - offsetY);
+
+      draw(mouseX, mouseY, 400);
+    }
+    draw(w / 2, h / 2, 400);
+  }
+</script>
+
+<style>
+  canvas {
+    position: fixed;
+  }
+</style>
+
+<!-- <script>
+  import { onMount } from "svelte";
   let canvas, ctx;
 
   onMount(() => {
@@ -27,10 +103,6 @@
       ctx.drawImage(background, 0, 0);
     };
   });
-</script>
-
-<style>
-
-</style>
-
-<canvas id="mask" />
+</script> -->
+<canvas id="canvasOne" width="500" height="300" />
+<img src="./img/dummy1.jpg" alt="" />
